3c32155cf711c2c358ddb9cbde387b38d0653805
lukas <lukasr101@gmail.com>
2023-06-08 12:59:48 +0200

    playing around with stuff. Kinda works

--- RustEval/src/main.rs
+++ RustEval/src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    // Constructing an expression: (One + Zero) * ETrue
+    let expr = E::Mult(
+        Box::new(E::Plus(Box::new(E::One), Box::new(E::Zero))),
+        Box::new(E::ETrue),
+    );
+
+    // Evaluating the expression
+    let result = expr.evaluate();
+
+    println!("Result: {}", result); // Output: Result: true
 }
+
+enum E {
+    One,
+    Zero,
+    ETrue,
+    EFalse,
+    Plus(Box<E>, Box<E>),
+    Mult(Box<E>, Box<E>),
+    Or(Box<E>, Box<E>),
+}
+
+impl E {
+    fn evaluate(&self) -> bool {
+        match self {
+            E::One => true,
+            E::Zero => false,
+            E::ETrue => true,
+            E::EFalse => false,
+            E::Plus(e1, e2) => e1.evaluate() || e2.evaluate(),
+            E::Mult(e1, e2) => e1.evaluate() && e2.evaluate(),
+            E::Or(e1, e2) => e1.evaluate() || e2.evaluate(),
+        }
+    }
+}
+
