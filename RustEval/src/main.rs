a1504f40aebb23aaa079e31d35e5fa83c5fc7340
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2023-06-08 15:18:18 +0200

    parser for arithmetic expressions from string to token

--- RustEval/src/main.rs
+++ RustEval/src/main.rs
@@ -12,14 +12,46 @@ pub enum Token {
     },
 }
 
-fn eval(e : &Token) -> i32 {
-    match e {
+fn eval(token : &Token) -> i32 {
+    match token {
         Token::Int { val } => return *val,
         Token::Plus { left, right } => return eval(left) + eval(right),
         Token::Mult { left, right } => return eval(left) * eval(right),
     }
 }
 
+fn parse(eval_string : &str) -> Token {
+    return if eval_string.contains("+") {
+        parse_plus(eval_string)
+    } else if eval_string.contains("*") {
+        parse_mult(eval_string)
+    } else {
+        parse_int(eval_string)
+    }
+}
+
+fn parse_mult(eval_string : &str) -> Token {
+    let parts = eval_string.split("*").collect::<Vec<_>>();
+    return Token::Mult { left: Box::new(parse(parts[0]))
+        , right: Box::new(parse(parts[1]))};
+}
+
+fn parse_plus(eval_string : &str) -> Token {
+    let parts = eval_string.split("+").collect::<Vec<_>>();
+    return Token::Plus { left: Box::new(parse(parts[0]))
+        , right: Box::new(parse(parts[1]))};
+}
+
+fn parse_int(eval_string : &str) -> Token {
+    return match eval_string.trim().parse::<i32>() {
+        Ok(n) => Token::Int { val: n },
+        Err(e) =>
+            {
+                eprintln!("Invalid input {}", eval_string);
+                Token::Int { val: 0 }
+            }
+    }
+}
 
 fn main() {
     {
@@ -30,5 +62,11 @@ fn main() {
     {
         let e = Token::Plus{ left: Box::new(Token::Int { val: 1 }), right: Box::new(Token::Int { val: 2})};
         println!("{}", eval(&e));
+    }
+
+    {
+        let str = "2 + 10 * 3";
+        let t = parse(&str);
+        println!("{}", eval(&t));
     }
 }
