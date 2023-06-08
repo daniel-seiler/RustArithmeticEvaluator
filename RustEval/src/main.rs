e2df08b6c5097804097deaf229116bbb8c431748
Daniel Seiler <73987329+daniel-seiler@users.noreply.github.com>
2023-06-08 13:57:37 +0200

    simple working example of expressions

--- RustEval/src/main.rs
+++ RustEval/src/main.rs
@@ -1,3 +1,34 @@
+pub enum E {
+    Int {
+        val: i32
+    },
+    Plus {
+        left: Box<E>,
+        right: Box<E>
+    },
+    Mult {
+        left: Box<E>,
+        right: Box<E>
+    },
+}
+
+fn eval(e : &E) -> i32 {
+    match e {
+        E::Int { val } => return *val,
+        E::Plus { left, right } => return eval(left) + eval(right),
+        E::Mult { left, right } => return eval(left) * eval(right),
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    {
+        let e = E::Int { val: 1 };
+        println!("{}", eval(&e));
+    }
+
+    {
+        let e = E::Plus{ left: Box::new(E::Int { val: 1 }), right: Box::new(E::Int { val: 2})};
+        println!("{}", eval(&e));
+    }
 }
